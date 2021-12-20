42a786d910887bd8454c427ab9f2a39b188602dd
Ed Williams <edwilliamsdevelopment@gmail.com>
2021-12-20 09:46:00 +0000

    added request interception

--- ssr.mjs
+++ ssr.mjs
@@ -4,21 +4,38 @@ import beautify from 'js-beautify'
 import { cmd } from './utils.mjs'
 import { getElement, switchBody } from './process-html.mjs'
 
+// launch browser / new page
 const browser = await puppeteer.launch()
 const page = await browser.newPage()
 
+// allows request interception; must appear before first page.goto()
+await page.setRequestInterception(true)
+
+// prevent certain scripts from firing
+page.on('request', (req) => {
+  const blocklist = ['analytics.js']
+  if (blocklist.find((regex) => req.url().match(regex))) {
+    return req.abort()
+  }
+  req.continue()
+})
+
+// goto page and wait to load
 const src = 'http://localhost:8080/index.html'
 
 await page.goto(src, { waitUntil: 'networkidle0' })
 
 await page.waitForSelector('#app')
 
+// store page content and close browser
 const html = await page.content()
 
 await browser.close()
 
+// extract just <body> element from HTML
 const body = await getElement({ tag: 'body', html })
 
+// create index-ssr.html w/ new populated <body> element
 await cmd('cp public/index.html public/index-ssr.html').catch(console.log)
 
 const indexHTML = fs.readFileSync('public/index.html').toString()
